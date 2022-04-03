2d8eb07d35884608c0752787c801af576885b4de
Houssei-up <63685988+Houssei-up@users.noreply.github.com>
2022-04-03 15:39:27 +0200

    Update Create_ct.sh

--- Create_ct.sh
+++ Create_ct.sh
@@ -1,1 +1,7 @@
+pct stop 102
+pct destroy 102
+IMAGE_ID=$(pveam list local | grep debian | cut -d" " -f1)
+CURRENT_ID=$(((100+$(pct list | wc -l))-1))
 
+echo "$3" > Filekey
+pct create $CURRENT_ID $IMAGE_ID --rootfs local-lvm:8 --hostname $1 --nameserver 8.8.8.8 --net0 name=eth0,bridge=vmbr0,gw=192.168.56.254,ip=$2 --ssh-public-keys Filekey --start 1
